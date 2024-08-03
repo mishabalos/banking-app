a2fb05e98ba2a6de7c2eab65a68f91d967b0be00
Misha Abalos <abalos.mishaela@gmail.com>
2024-08-03 12:25:35 +0800

    updated logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Login.css'; 
+import logo from "../assets/images/avionbank-logo.png"
 
 const Login = () => {
     return (
     <div className="login-container">
         <div className="login-box">
-            <img src="/assets/images/avionbank-logo.png" alt="Avion Bank Logo" className="logo" />
+            <img src={logo} alt="Avion Bank Logo" className="logo" />
             <h2>Welcome Back!</h2>
             <h3>Let's Sign You In.</h3>
             <form>
