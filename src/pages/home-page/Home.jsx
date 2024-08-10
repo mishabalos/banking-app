3cbf33cdd3aed7763cdac99f834c91f9faab253f
Misha Abalos <abalos.mishaela@gmail.com>
2024-08-10 11:47:49 +0800

    Updated home page

--- src/pages/home-page/Home.jsx
+++ src/pages/home-page/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import './Home.css'; 
+import bankUsers from '../../assets/data/bankUsers.json';
+// import { useNavigate } from 'react-router-dom'; 
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>Homepage works!</h1>
-    </div>
-  );
+    <div className="home-container">
+       <h1>Accounts</h1>
+        {bankUsers.map((account, index) => (
+          <div className="bankUsers-box" key={index}>
+            <h2>Account Number: {account.accountNumber}</h2>
+            <p>{account.accountName}</p>
+            <p>{account.email}</p>
+            <p className="totalBalance">Total Balance: {account.totalBalance}</p>
+            </div> 
+        ))}
+        </div>
+    ); 
 };
 
 export default Home;
