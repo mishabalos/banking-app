d7dd65106e239bc2f88949c73a7f14b1bee6cb8d
Misha Abalos <abalos.mishaela@gmail.com>
2024-08-17 09:36:40 +0800

    sidenav

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,37 +1,67 @@
-// src/components/SideNav.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SideNav.css';
-import logo from "../../assets/images/avionbank-logo-3.png"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import logo from '../../assets/images/avionbank-logo-3.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const SideNav = () => {
   const navigate = useNavigate();
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  // const [accountType, setAccountType] = useState(null);
+
+  let accountDefault 
+
+    if (currentUser.accountType === 'client') {
+    accountDefault = [currentUser];
+  } else {
+    accountDefault = JSON.parse(localStorage.getItem('users'));
+  }
+
+  // useEffect(() => {
+  //   // Get the account type from localStorage (or any other method)
+  //   const storedAccountType = localStorage.getItem('accountType');
+  //   setAccountType(storedAccountType);
+  // }, []);
+
 
   const handleLogout = () => {
     // Clear user session data (if stored in localStorage or cookies)
     localStorage.removeItem('userSession'); // Example: clear user session data
-
+    localStorage.removeItem('accountType'); // Clear the account type as well
     // Redirect to login page
     navigate('/');
   };
 
   return (
     <div className="sidenav">
-      <div className='sidenav-menu'>
-      <div className="sidenav-logo">
-      <img src={logo} alt="Avion Bank Logo" />
+      <div className="sidenav-menu">
+        <div className="sidenav-logo">
+          <img src={logo} alt="Avion Bank Logo" />
         </div>
-      <ul>
-        <li><a href="/home">Accounts</a></li>
-        <li><a href="/transaction">Transactions</a></li>
-        <li><a href="/usercreation">Create Account</a></li>
-        <li><a href="/budget">Budget App</a></li>
-      </ul>   
+        <ul>
+          <li>
+            <a href="/home">Accounts</a>
+          </li>
+          {currentUser.accountType === 'admin' && (
+            <>
+              <li>
+                <a href="/transaction">Transactions</a>
+              </li>
+              <li>
+                <a href="/usercreation">Create Account</a>
+              </li>
+            </>
+          )}
+          <li>
+            <a href="/budget">Budget App</a>
+          </li>
+        </ul>
       </div>
-      <div className='sidenav-account'>
-      <a href="/" onClick={handleLogout}><FontAwesomeIcon icon={faRightFromBracket} />Logout</a>
+      <div className="sidenav-account">
+        <a href="/" onClick={handleLogout}>
+          <FontAwesomeIcon icon={faRightFromBracket} /> Logout
+        </a>
       </div>
     </div>
   );
